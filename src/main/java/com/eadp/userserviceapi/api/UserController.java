e013e68c5f5cdd83d19e2c00d2a4ca4ffd39d31d
ozen-hub <info.ozenhub@gmail.com>
2024-01-23 20:39:15 +0530

    1

---
+++ src/main/java/com/eadp/userserviceapi/api/UserController.java
@@ -0,0 +1,29 @@
+package com.eadp.userserviceapi.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+
+    @PostMapping
+    public String createUser(){
+        return "createUser";
+    }
+    @PutMapping
+    public String updateUser(){
+        return "updateUser";
+    }
+    @GetMapping
+    public String findUser(){
+        return "findUser";
+    }
+    @DeleteMapping
+    public String deleteUser(){
+        return "deleteUser";
+    }
+    @GetMapping("/list")
+    public String findAllUsers(){
+        return "findAllUsers";
+    }
+}
