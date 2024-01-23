9c752d2d9d8933695ad8a5a3aacfe2e4eb3bcaae
ozen-hub <info.ozenhub@gmail.com>
2024-01-23 22:22:00 +0530

    10

--- src/main/java/com/eadp/userserviceapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/eadp/userserviceapi/service/impl/UserServiceImpl.java
@@ -6,25 +6,35 @@ import com.eadp.userserviceapi.dto.response.ResponseUserDto;
 import com.eadp.userserviceapi.entity.User;
 import com.eadp.userserviceapi.repo.UserRepo;
 import com.eadp.userserviceapi.service.UserService;
+import com.eadp.userserviceapi.util.KeyManager;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
 
     private final UserRepo userRepo;
+    private final KeyManager keyManager;
 
     @Autowired
-    public UserServiceImpl(UserRepo userRepo) {
+    public UserServiceImpl(UserRepo userRepo, KeyManager keyManager) {
         this.userRepo = userRepo;
+        this.keyManager = keyManager;
     }
 
     @Override
     public void createUser(RequestUserDto dto) {
         User user = new User();
-
+        user.setUserId(keyManager.generateKey(new Random().nextInt(15)));
+        user.setFullName(dto.getFullName());
+        user.setEmail(dto.getEmail());
+        user.setStatus(true);
+        user.setAvatarUrl(dto.getAvatarUrl().getBytes());
+        userRepo.save(user);
     }
 
     @Override
