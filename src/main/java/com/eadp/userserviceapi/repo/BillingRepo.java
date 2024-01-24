9113f5fbc46e6aff8fa1281d33ca4ce88642bccd
ozen-hub <info.ozenhub@gmail.com>
2024-01-24 21:55:27 +0530

    18

--- src/main/java/com/eadp/userserviceapi/repo/BillingRepo.java
+++ src/main/java/com/eadp/userserviceapi/repo/BillingRepo.java
@@ -1,10 +1,14 @@
 package com.eadp.userserviceapi.repo;
 
 import com.eadp.userserviceapi.entity.BillingAddress;
+import com.eadp.userserviceapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @EnableJpaRepositories
 public interface BillingRepo extends JpaRepository<BillingAddress, Long> {
+    public Optional<BillingAddress> findBillingAddressByUser(User user);
 }
--- src/main/java/com/eadp/userserviceapi/service/impl/BillingAddressServiceImpl.java
+++ src/main/java/com/eadp/userserviceapi/service/impl/BillingAddressServiceImpl.java
@@ -2,20 +2,54 @@ package com.eadp.userserviceapi.service.impl;
 
 import com.eadp.userserviceapi.dto.common.BillingAddressDto;
 
+import com.eadp.userserviceapi.entity.BillingAddress;
+import com.eadp.userserviceapi.entity.User;
+import com.eadp.userserviceapi.repo.BillingRepo;
+import com.eadp.userserviceapi.repo.UserRepo;
 import com.eadp.userserviceapi.service.BillingAddressService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class BillingAddressServiceImpl implements BillingAddressService {
 
+    private final BillingRepo billingRepo;
+    private final UserRepo userRepo;
+
+    @Autowired
+    public BillingAddressServiceImpl(BillingRepo billingRepo, UserRepo userRepo) {
+        this.billingRepo = billingRepo;
+        this.userRepo = userRepo;
+    }
+
     @Override
     public void saveAddress(BillingAddressDto dto, String userId) {
 
+        Optional<User> selectedUser = userRepo.findUserByUserId(userId);
+        if (selectedUser.isEmpty()) throw new RuntimeException();
+
+        BillingAddress billingAddress = new BillingAddress();
+        billingAddress.setCountry(dto.getCountry());
+        billingAddress.setCity(dto.getCity());
+        billingAddress.setZip(dto.getZip());
+        billingAddress.setUser(selectedUser.get());
+        billingRepo.save(billingAddress);
     }
 
     @Override
     public void updateAddress(BillingAddressDto dto, String userId) {
+        Optional<User> selectedUser = userRepo.findUserByUserId(userId);
+        if (selectedUser.isEmpty()) throw new RuntimeException();
 
+        Optional<BillingAddress> selectedBillingAddress = billingRepo.findBillingAddressByUser(selectedUser.get());
+        if (selectedBillingAddress.isEmpty()) throw new RuntimeException();
+
+        selectedBillingAddress.get().setCountry(dto.getCountry());
+        selectedBillingAddress.get().setCity(dto.getCity());
+        selectedBillingAddress.get().setZip(dto.getZip());
+        billingRepo.save(selectedBillingAddress.get());
     }
 
     @Override
